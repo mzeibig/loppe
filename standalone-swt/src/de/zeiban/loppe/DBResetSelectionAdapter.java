5560f119064a4725d37ebcd49feea322f26dd0c7
heikea <heikea@ada6b997-eca0-46c5-a190-986f6ee60589>
2011-09-15 11:51:07 +0000

    fixes#7 DB reset

---
+++ standalone-swt/src/de/zeiban/loppe/DBResetSelectionAdapter.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package de.zeiban.loppe;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.swt.widgets.Shell;
+
+import de.zeiban.loppe.dbcore.*;
+
+final class DBResetSelectionAdapter extends SelectionAdapter {
+	private final Shell shell;
+	private final DbTemplate dbTemplate;
+
+	public DBResetSelectionAdapter(final Shell shell, final Connection connection) {
+		this.shell = shell;
+		dbTemplate = new DbTemplate(connection);
+	}
+
+	@Override
+	public void widgetSelected(final SelectionEvent e) {
+		final MessageBox messageBox = new MessageBox(shell, SWT.ICON_QUESTION|SWT.YES|SWT.NO);
+		messageBox.setMessage("Datenbank jetzt zurücksetzen ?\n!!!Achtung: Alle Daten werden gelöscht !!!");
+		if (messageBox.open() == SWT.YES) {   
+			final MessageBox confirmMessageBox = new MessageBox(shell, SWT.ICON_QUESTION|SWT.YES|SWT.NO);
+			confirmMessageBox.setMessage("Wirklich alle Daten aus der Datenbank löschen ?");
+			if (confirmMessageBox.open() == SWT.YES) {
+				//dbTemplate.execute("delete from kauf");
+			}
+
+		}
+	}
+}
